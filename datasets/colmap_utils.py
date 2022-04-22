acbc7249f71678ad9cd1310b374b6522b8ad54dd
Alberto <albertoceballosgonzales@alteia.com>
2022-04-22 16:09:56 +0200

    updated

--- datasets/colmap_utils.py
+++ datasets/colmap_utils.py
@@ -265,6 +265,7 @@ def read_points3d_binary(path_to_model_file):
             image_ids = np.array(image_ids_list)
             point2D_idxs = np.array(tuple(map(int, track_elems[1::2])))
             if len(image_ids_list)>0:
+                print(image_ids)
                 points3D[point3D_id] = Point3D(
                 	   id=point3D_id, xyz=xyz, rgb=rgb,
                 	      error=error, image_ids=image_ids,
