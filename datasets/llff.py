b4699ca08bbc25711f48c38c97b618ef6cc2c2e9
Alberto <albertoceballosgonzales@alteia.com>
2022-04-26 15:25:44 +0200

    c

--- datasets/llff.py
+++ datasets/llff.py
@@ -177,7 +177,7 @@ class LLFFDataset(Dataset):
 
     def read_meta(self):
 
-        selected = [1,2,3,5,7,8,9,10,
+        """selected = [1,2,3,5,7,8,9,10,
                     11,12,13,14,15,16,17,18,19,20,
                     21,25,26,27,28,29,30,
                     31,32,33,34,36,37,38,39,40,
@@ -185,7 +185,9 @@ class LLFFDataset(Dataset):
                     51,52,53,54,55,56,57,60,
                     61,62,63,64,66,67,69,70,
                     71,72,75,76,77,78,79,80,
-                    81,83]
+                    81,83]"""
+
+        selected = [2,7,14,17,18,21,23,27,51,56]
 
         imdata, camlist = read_images(os.path.join(self.root_dir, 'sparse/0/images.bin'), selected, True)
         # Step 1: rescale focal length according to training resolution
